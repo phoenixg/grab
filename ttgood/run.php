c6884876a38d9f5396b65a7350d4e679156cbdf6
phoenixg-compaq321 <306761352@qq.com>
2013-01-05 22:56:47 +0800

    1111

--- ttgood/run.php
+++ ttgood/run.php
@@ -1,4 +1,39 @@
 <?php 
 
+/*
+jy_f1 - jy_f199 每页有15条教员记录
 
-echo __FILE__;
+2013-01-05
+
+*/
+
+require ('./simplehtmldom_1_5/simple_html_dom.php');
+
+define(BASE,   'http://www.ttgood.com/');
+
+// http://www.ttgood.com/jy/t10072.htm
+
+
+
+
+$mentorList  = array();
+for ($i=1; $i < 200; $i++) { 
+	$html = file_get_html(BASE . "jy_f{$i}/");
+	foreach($html->find('table .orange_') as $mentor)
+	   $mentorList[] = BASE . trim(str_replace('../', '', $mentor->href));
+}
+
+echo '<pre>';print_r($mentorList);
+
+
+
+
+
+
+
+
+
+
+
+
+
