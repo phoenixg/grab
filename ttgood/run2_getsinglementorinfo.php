7bb8f113200db846973d0b36f427865a6ea0efd4
phoenixg-compaq321 <306761352@qq.com>
2013-01-06 21:46:47 +0800

    afra

--- ttgood/run2_getsinglementorinfo.php
+++ ttgood/run2_getsinglementorinfo.php
@@ -22,8 +22,15 @@ define(BASE,   'http://www.ttgood.com/');
 
 set_time_limit(0);
 
-//$html = file_get_html('http://www.ttgood.com/jy/t138288.htm');
+// 非教员
 $html = file_get_html('http://www.ttgood.com/jy/t150097.htm');
+
+// 教员
+$html = file_get_html('http://www.ttgood.com/jy/t10072.htm');
+
+/* 
+最近登录时间，有时有 Fatal error: Call to a member function find() on a non-object 错误
+所以，还是在列表里采集好了
 
 $mentor_lastLoginTime = trim($html->find("table", 3)
                                   ->find("tr td", 2)
@@ -33,6 +40,7 @@ $mentor_lastLoginTime = trim($html->find("table", 3)
                                   ->find("td", 1)
                                   ->find("td", 3)
                                   ->find("span", 0)->plaintext);
+*/
 
 $mentor_sex           = trim($html->find("table", 3)
                                   ->find("tr td", 2)
@@ -41,6 +49,7 @@ $mentor_sex           = trim($html->find("table", 3)
                                   ->find("tr", 3)
                                   ->find("div", 1)->plaintext);
 
+echo $mentor_sex . '<br />';
 
 $mentor_nationality   = trim($html->find("table", 3)
                                   ->find("tr td", 2)
@@ -49,6 +58,8 @@ $mentor_nationality   = trim($html->find("table", 3)
                                   ->find("tr", 3)
                                   ->find("td", 3)->plaintext);
 
+echo $mentor_nationality. '<br />';
+
 $mentor_birthday      = trim($html->find("table", 3)
                                   ->find("tr td", 2)
                                   ->find("table tr", 1)
@@ -56,6 +67,7 @@ $mentor_birthday      = trim($html->find("table", 3)
                                   ->find("tr", 4)
                                   ->find("td", 1)->plaintext);
 
+echo $mentor_birthday . '<br />';
 
 $mentor_school        = trim($html->find("table", 3)
                                   ->find("tr td", 2)
@@ -64,7 +76,7 @@ $mentor_school        = trim($html->find("table", 3)
                                   ->find("tr", 4)
                                   ->find("td", 3)->plaintext);
 
-
+echo $mentor_school . '<br />';
 
 $mentor_academic      = trim($html->find("table", 3)
                                   ->find("tr td", 2)
@@ -73,6 +85,7 @@ $mentor_academic      = trim($html->find("table", 3)
                                   ->find("tr", 5)
                                   ->find("td", 1)->plaintext);
 
+echo $mentor_academic . '<br />';
 
 $mentor_major         = trim($html->find("table", 3)
                                   ->find("tr td", 2)
@@ -81,6 +94,8 @@ $mentor_major         = trim($html->find("table", 3)
                                   ->find("tr", 5)
                                   ->find("td", 3)->plaintext);
 
+echo $mentor_major . '<br />';
+
 $mentor_identity      = trim($html->find("table", 3)
                                   ->find("tr td", 2)
                                   ->find("table tr", 1)
@@ -88,6 +103,7 @@ $mentor_identity      = trim($html->find("table", 3)
                                   ->find("tr", 6)
                                   ->find("td", 1)->plaintext);
 
+echo $mentor_identity . '<br />';
 
 $mentor_teachable     = trim($html->find("table", 3)
                                   ->find("tr td", 2)
@@ -96,7 +112,7 @@ $mentor_teachable     = trim($html->find("table", 3)
                                   ->find("tr", 1)
                                   ->find("td", 1)->plaintext);
 
-
+echo $mentor_teachable . '<br />';
 
 $mentor_selfIntro     = trim($html->find("table", 3)
                                   ->find("tr td", 2)
@@ -105,15 +121,16 @@ $mentor_selfIntro     = trim($html->find("table", 3)
                                   ->find("tr", 2)
                                   ->find("td", 1)->plaintext);
 
-
+echo $mentor_selfIntro . '<br />';
 
 $mentor_certificate   = trim($html->find("table", 3)
                                   ->find("tr td", 2)
                                   ->find("table tr", 1)
                                   ->find("table", 3)
                                   ->find("tr", 3)
-                                  ->find("td", 1));
+                                  ->find("td", 1)->plaintext);
 
+echo $mentor_certificate . '<br />';
 
 /*
 $html->find("table", 3)
@@ -121,3 +138,8 @@ $html->find("table", 3)
                                   ->find("table tr", 1)
                                   ->find("table", 2)
 */
+
+
+
+
+
