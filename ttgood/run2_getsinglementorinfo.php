54f0fac50f33ce8da9f931ede4561546a002bdbb
phoenixg-compaq321 <306761352@qq.com>
2013-01-07 20:56:34 +0800

    jijij

--- ttgood/run2_getsinglementorinfo.php
+++ ttgood/run2_getsinglementorinfo.php
@@ -16,6 +16,28 @@ header("Content-type: text/html; charset=GB2312");
 
 */
 
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
+date_default_timezone_set('Asia/Shanghai');
+
+define('EOL',(PHP_SAPI == 'cli') ? PHP_EOL : '<br />');
+
+require_once './PHPExcel_1_7_8/Classes/PHPExcel.php';
+
+$objPHPExcel = new PHPExcel();
+
+$objPHPExcel->getProperties()->setCreator("黄峰")
+                 ->setLastModifiedBy("黄峰")
+                 ->setTitle("ttgood采集结果")
+                 ->setSubject("ttgood采集结果")
+                 ->setDescription("ttgood采集结果")
+                 ->setKeywords("ttgood 家教")
+                 ->setCategory("ttgood采集结果");
+
+
+
+
 require ('./simplehtmldom_1_5/simple_html_dom.php');
 
 define('BASE',   'http://www.ttgood.com/');
@@ -23,10 +45,10 @@ define('BASE',   'http://www.ttgood.com/');
 set_time_limit(0);
 
 // 无教师资格的教员
-$html = file_get_html('http://www.ttgood.com/jy/t150097.htm');
+$html = file_get_html('http://www.ttgood.com/jy/t144494.htm');
 
 // 有教师资格的教员
-$html = file_get_html('http://www.ttgood.com/jy/t56369.htm');
+//$html = file_get_html('http://www.ttgood.com/jy/t148686.htm');
 
 /* 
 最近登录时间，有时有 Fatal error: Call to a member function find() on a non-object 错误
@@ -49,7 +71,7 @@ $mentor_sex           = trim($html->find("table", 3)
                                   ->find("tr", 3)
                                   ->find("div", 1)->plaintext);
 
-echo $mentor_sex . '<br />';
+//echo $mentor_sex . '<br />';
 
 $mentor_nationality   = trim($html->find("table", 3)
                                   ->find("tr td", 2)
@@ -58,7 +80,7 @@ $mentor_nationality   = trim($html->find("table", 3)
                                   ->find("tr", 3)
                                   ->find("td", 3)->plaintext);
 
-echo $mentor_nationality. '<br />';
+//echo $mentor_nationality. '<br />';
 
 $mentor_birthday      = trim($html->find("table", 3)
                                   ->find("tr td", 2)
@@ -67,7 +89,7 @@ $mentor_birthday      = trim($html->find("table", 3)
                                   ->find("tr", 4)
                                   ->find("td", 1)->plaintext);
 
-echo $mentor_birthday . '<br />';
+//echo $mentor_birthday . '<br />';
 
 $mentor_school        = trim($html->find("table", 3)
                                   ->find("tr td", 2)
@@ -76,7 +98,7 @@ $mentor_school        = trim($html->find("table", 3)
                                   ->find("tr", 4)
                                   ->find("td", 3)->plaintext);
 
-echo $mentor_school . '<br />';
+//echo $mentor_school . '<br />';
 
 $mentor_academic      = trim($html->find("table", 3)
                                   ->find("tr td", 2)
@@ -85,7 +107,7 @@ $mentor_academic      = trim($html->find("table", 3)
                                   ->find("tr", 5)
                                   ->find("td", 1)->plaintext);
 
-echo $mentor_academic . '<br />';
+//echo $mentor_academic . '<br />';
 
 $mentor_major         = trim($html->find("table", 3)
                                   ->find("tr td", 2)
@@ -94,7 +116,7 @@ $mentor_major         = trim($html->find("table", 3)
                                   ->find("tr", 5)
                                   ->find("td", 3)->plaintext);
 
-echo $mentor_major . '<br />';
+//echo $mentor_major . '<br />';
 
 $mentor_identity      = trim($html->find("table", 3)
                                   ->find("tr td", 2)
@@ -103,7 +125,7 @@ $mentor_identity      = trim($html->find("table", 3)
                                   ->find("tr", 6)
                                   ->find("td", 1)->plaintext);
 
-echo $mentor_identity . '<br />';
+//echo $mentor_identity . '<br />';
 
 $mentor_teachable     = trim($html->find("table", 3)
                                   ->find("tr td", 2)
@@ -112,7 +134,7 @@ $mentor_teachable     = trim($html->find("table", 3)
                                   ->find("tr", 1)
                                   ->find("td", 1)->plaintext);
 
-echo $mentor_teachable . '<br />';
+//echo $mentor_teachable . '<br />';
 
 $mentor_selfIntro     = trim($html->find("table", 3)
                                   ->find("tr td", 2)
@@ -121,7 +143,7 @@ $mentor_selfIntro     = trim($html->find("table", 3)
                                   ->find("tr", 2)
                                   ->find("td", 1)->plaintext);
 
-echo $mentor_selfIntro . '<br />';
+//echo $mentor_selfIntro . '<br />';
 
 $mentor_certificate   = trim($html->find("table", 3)
                                   ->find("tr td", 2)
@@ -130,10 +152,7 @@ $mentor_certificate   = trim($html->find("table", 3)
                                   ->find("tr", 3)
                                   ->find("td", 1)->plaintext);
 
-echo $mentor_certificate . '<br />';
-
-
-
+//echo $mentor_certificate . '<br />';
 
 $mentor_teachSchoolFlag = trim($html->find("table", 3)
                                   ->find("tr td", 2)
@@ -156,7 +175,7 @@ if ($mentor_teachSchoolFlag == mb_convert_encoding ('执教学校：', 'GB2312' 
                                   ->find("tr", 7)
                                   ->find("td", 1)->plaintext);
   
-  echo $mentor_teachSchool . '<br />';
+  //echo $mentor_teachSchool . '<br />';
 
   $mentor_teachSubject = trim($html->find("table", 3)
                                   ->find("tr td", 2)
@@ -165,7 +184,7 @@ if ($mentor_teachSchoolFlag == mb_convert_encoding ('执教学校：', 'GB2312' 
                                   ->find("tr", 7)
                                   ->find("td", 3)->plaintext);
 
-  echo $mentor_teachSubject . '<br />';
+  //echo $mentor_teachSubject . '<br />';
 
   $mentor_teachAge = trim($html->find("table", 3)
                                   ->find("tr td", 2)
@@ -174,7 +193,7 @@ if ($mentor_teachSchoolFlag == mb_convert_encoding ('执教学校：', 'GB2312' 
                                   ->find("tr", 8)
                                   ->find("td", 1)->plaintext);
 
-  echo $mentor_teachAge . '<br />';
+  //echo $mentor_teachAge . '<br />';
 
   $mentor_teachLevel = trim($html->find("table", 3)
                                   ->find("tr td", 2)
@@ -183,11 +202,39 @@ if ($mentor_teachSchoolFlag == mb_convert_encoding ('执教学校：', 'GB2312' 
                                   ->find("tr", 8)
                                   ->find("td", 3)->plaintext);
 
-  echo $mentor_teachLevel . '<br />';
+  //echo $mentor_teachLevel . '<br />';
 
 }
 
 
+
+$objPHPExcel->setActiveSheetIndex(0)
+             ->setCellValue('D2', iconv('gb2312', 'utf-8', $mentor_sex))
+             ->setCellValue('E2', iconv('gb2312', 'utf-8', $mentor_nationality))
+             ->setCellValue('F2', iconv('gb2312', 'utf-8', $mentor_birthday))
+             ->setCellValue('G2', iconv('gb2312', 'utf-8', $mentor_school))
+             ->setCellValue('H2', iconv('gb2312', 'utf-8', $mentor_academic))
+             ->setCellValue('I2', iconv('gb2312', 'utf-8', $mentor_major))
+             ->setCellValue('J2', iconv('gb2312', 'utf-8', $mentor_identity))
+             ->setCellValue('K2', iconv('gb2312', 'utf-8', $mentor_teachable))
+             ->setCellValue('L2', iconv('gb2312', 'utf-8', $mentor_selfIntro))
+             ->setCellValue('M2', iconv('gb2312', 'utf-8', $mentor_certificate))
+             ->setCellValue('N2', iconv('gb2312', 'utf-8', $mentor_teachSchool))
+             ->setCellValue('O2', iconv('gb2312', 'utf-8', $mentor_teachSubject))
+             ->setCellValue('P2', iconv('gb2312', 'utf-8', $mentor_teachAge))
+             ->setCellValue('Q2', iconv('gb2312', 'utf-8', $mentor_teachLevel));
+
+
+
+
+
+$objPHPExcel->getActiveSheet()->setTitle('result');
+
+$objPHPExcel->setActiveSheetIndex(0);
+
+
+$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+$objWriter->save(str_replace('.php', '.xlsx', __FILE__));
 
 
 
