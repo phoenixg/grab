b2c614c9382c34ea688df1fb223d9f3c9f46d436
phoenix <306761352@qq.com>
2013-01-11 11:08:13 +0800

    备份一下

--- ttgood/backup3/run1.0_getmentorlist_seg1.php
+++ ttgood/backup3/run1.0_getmentorlist_seg1.php
@@ -8,8 +8,6 @@ ini_set('display_errors', TRUE);
 ini_set('display_startup_errors', TRUE);
 date_default_timezone_set('Asia/Shanghai');
 
-define('EOL',(PHP_SAPI == 'cli') ? PHP_EOL : '<br />');
-
 require_once '../PHPExcel_1_7_8/Classes/PHPExcel.php';
 
 $objPHPExcel = new PHPExcel();
@@ -23,9 +21,9 @@ define('BASE',   'http://www.ttgood.com/');
 set_time_limit(0);
 
 $mentorList  = array();
-for ($j=1104; $j < 6517; $j++) {  
+for ($j=1105; $j < 1500; $j++) {  
 
-   $objPHPExcel = PHPExcel_IOFactory::load("01.xlsx");
+   $objPHPExcel = PHPExcel_IOFactory::load("01_seg1.xlsx");
 
     $html = file_get_html(BASE . "shanghai/teacher_info/index_all.php?taxis=0&total_pages=6518&now_page={$j}/");
     
@@ -35,6 +33,8 @@ for ($j=1104; $j < 6517; $j++) {
                                                                             ->find("tr", $i+1) //第1行是标题，教员信息从第2行开始，一共1-12有12条教员记录
                                                                             ->find("div", 4)->plaintext);
 
+        //echo $mentor_lastLoginTime;
+      
         $mentor_linkTmp             =  trim($html->find("table", 5)
                                                                             ->find("td", 2)
                                                                             ->find("tr", $i+1)
@@ -43,11 +43,12 @@ for ($j=1104; $j < 6517; $j++) {
         $mentor_link = BASE . trim(str_replace('../', '', $mentor_linkTmp));
         //$mentorList[] = array($mentor_lastLoginTime, $mentor_link);
 
-        $objPHPExcel->setActiveSheetIndex(0)
-                                     ->setCellValue('A'.(12*($j-1)+$i+1), $mentor_linkTmp)
-                                     ->setCellValue('B'.(12*($j-1)+$i+1), $mentor_lastLoginTime);
-    }
+        //echo $mentor_link;
 
+        $objPHPExcel->setActiveSheetIndex(0)
+                                     ->setCellValue('A'.(12*($j-1)+$i+1), iconv('gb2312', 'utf-8', $mentor_linkTmp))
+                                     ->setCellValue('B'.(12*($j-1)+$i+1), iconv('gb2312', 'utf-8', $mentor_lastLoginTime));
+    }
 
 
     //$objPHPExcel->getActiveSheet()->setTitle('result');
@@ -56,7 +57,7 @@ for ($j=1104; $j < 6517; $j++) {
 
 
     $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
-    $objWriter->save(str_replace('.php', '.xlsx', "01.xlsx"));
+    $objWriter->save(str_replace('.php', '.xlsx', "01_seg1.xlsx"));
 
 
       // 释放内存的感觉真好，可以一次全部抓取下来！
