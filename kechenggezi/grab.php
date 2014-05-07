0d29388d594b12814bfdcb3828db7cd5c9226153
phoenixg <306761352@qq.com>
2014-05-07 11:30:10 +0800

    a

--- kechenggezi/grab.php
+++ kechenggezi/grab.php
@@ -1,34 +1,37 @@
 <?php 
 
+set_time_limit(100);
+
+
 $url = 'http://kechenggezi.com/users/';
 $urlAppend = '/details.json?token=';
 
 // 从Fiddler那里，手机访问课程格子，抓手机包来获得token
 $token = 'NYMVRFHLJRGEGSODFJEPQK';
 
-$userId = '1000';
+$result = array();
+for ($userId = 1; $userId < 100; $userId ++) { 
+    $urlFull = $url . $userId . $urlAppend . $token;
 
-$urlFull = $url . $userId . $urlAppend . $token;
+    $handle = curl_init($urlFull);
+    curl_setopt($handle,  CURLOPT_RETURNTRANSFER, TRUE);
+    $response = curl_exec($handle);
+    $httpCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+    if($httpCode == 404) {
+        continue;
+    }
 
-$json = file_get_contents($urlFull);
-$obj = json_decode($json);
-$objUser = $obj->user;
+    curl_close($handle);
 
-echo $objUser->tiny_avatar_url;
-
-
-/*
-
-
-if( 404 ) {
-	next;
-} else {
-	$arr[] = array('id' => '', 'name' => '')
+    $json = file_get_contents($urlFull);
+    $obj = json_decode($json);
+    $objUser = $obj->user;
+    $result[] = $objUser->tiny_avatar_url;
 }
 
-foreach($arr as $people){
-	// print out as a tr line
-}
+print_r($result);die;
 
-*/
+
+
+
 
