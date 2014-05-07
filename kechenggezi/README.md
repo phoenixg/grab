504a51eee6d392d8f8a236fe3b34bd130636fbd7
phoenixg <306761352@qq.com>
2014-05-07 11:21:06 +0800

    add kecheng gezi

---
+++ kechenggezi/README.md
@@ -0,0 +1,1 @@
+http://www.hackdig.com/?05/hack-9785.htm
---
+++ kechenggezi/grab.php
@@ -0,0 +1,31 @@
+<?php 
+
+$url = 'http://kechenggezi.com/users/';
+$urlAppend = '/details.json?token=';
+
+// 从Fiddler那里，手机访问课程格子，抓手机包来获得token
+$token = 'NYMVRFHLJRGEGSODFJEPQK';
+
+$userId = '1000';
+
+$urlFull = $url . $userId . $urlAppend . $token;
+
+$json = file_get_contents($urlFull);
+$obj = json_decode($json);
+echo $obj->tiny_avatar_url;
+
+/*
+
+
+if( 404 ) {
+	next;
+} else {
+	$arr[] = array('id' => '', 'name' => '')
+}
+
+foreach($arr as $people){
+	// print out as a tr line
+}
+
+
+
