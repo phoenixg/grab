46918ee304d692a6a7863f752c6e6ab05dc48ce5
phoenixg <306761352@qq.com>
2014-05-07 17:15:39 +0800

    a

--- kechenggezi/php-long-polling/server/server.php
+++ kechenggezi/php-long-polling/server/server.php
@@ -3,7 +3,7 @@ header('Content-Type: text/html; charset=utf-8');
 
 set_time_limit(0);
 
-// user id 最大值 7261450 （截至 20140507）
+// user id 最大值 7261450 ； 最小值 27 （截至 20140507）
 // http://kechenggezi.com/users/7261450/details.json?token=NYMVRFHLJRGEGSODFJEPQK
 // http://kechenggezi.com/users/1000/details.json?token=NYMVRFHLJRGEGSODFJEPQK
 $url = 'http://kechenggezi.com/users/';
@@ -30,14 +30,23 @@ while( true ) {
         $response = curl_exec($handle);
         $httpCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);
 
-        if($httpCode == 404 || $userId == 1) {
-            $json = json_encode(array('data_from_file' => 'c', 'userId' => $userId));
+        if($httpCode == 404 || $userId < 25) {
+            $json = json_encode(array('data_from_file' => '遇到404啦亲', 'userId' => $userId));
             echo $json;
             curl_close($handle);
             break;
         } else {
             $json = file_get_contents($urlFull);
             $obj = json_decode($json);
+
+            // 可能是请求被拒绝！请求频率过高！
+            if(isset($obj->success) && $obj->success == false){
+                $json = json_encode(array('data_from_file' => '请求频率过高亲', 'userId' => $userId));
+                echo $json;
+                curl_close($handle);
+                break;
+            } 
+
             $objUser = $obj->user;
             $aaa = $objUser->tiny_avatar_url;
 
@@ -67,4 +76,12 @@ while( true ) {
 }
 
 
+/*
+{
 
+    "success": false,
+    "error": "请求被拒绝！请求频率过高！",
+    "notice": "请求被拒绝！请求频率过高！"
+
+}
+*/
